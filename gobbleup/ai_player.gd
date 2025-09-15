9977d53de2079f2e4b226e26dd7d4ac2c06f059c
rossmck <invrossmckechnie@gmail.com>
2025-09-15 21:42:36 +0100

    added AI and new player

---
+++ gobbleup/ai_player.gd
@@ -0,0 +1,27 @@
+extends CharacterBody3D
+
+@export var speed = 8.0 
+@export var friction = 0.1
+@export var max_size = 100
+
+var size = 0
+var target = null
+
+func _ready():
+	print("AI Turkey spawned.")
+	add_to_group("players") 
+	target = get_tree().get_first_node_in_group("player_1")
+
+func _physics_process(delta):
+	var direction = Vector3.ZERO
+	
+	if is_instance_valid(target):
+		direction = (target.global_position - global_position).normalized()
+	if direction:
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed * friction)
+		velocity.z = move_toward(velocity.z, 0, speed * friction)
+
+	move_and_slide()
